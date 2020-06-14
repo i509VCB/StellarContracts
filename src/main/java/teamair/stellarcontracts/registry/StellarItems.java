c78d459babecd24824262de5a8456c665a712296
Maowcraft <28311382+Maowcraft@users.noreply.github.com>
2020-06-14 11:34:54 -0400

    update hurrr

--- src/main/java/teamair/stellarcontracts/registry/StellarItems.java
+++ src/main/java/teamair/stellarcontracts/registry/StellarItems.java
@@ -5,24 +5,22 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 import teamair.stellarcontracts.StellarContracts;
 import teamair.stellarcontracts.item.CommunicatorItem;
 
 public final class StellarItems {
     // TODO: Set our proper item for this
+    // Items
+    public static final Item COMMUNICATOR = register("communicator", new CommunicatorItem(new Item.Settings()));
     // Groups
     public static final ItemGroup STELLAR_ITEM_GROUP = FabricItemGroupBuilder.build(StellarContracts.id("item_group"), () -> new ItemStack(Items.GLOWSTONE));
-    // Items
-    public static final Item COMMUNICATOR = new CommunicatorItem(new Item.Settings());
 
     private static Item register(String path, Item item) {
         return Registry.register(Registry.ITEM, StellarContracts.id(path), item);
     }
 
     static void init() {
-        register("communicator", COMMUNICATOR);
     }
 
     private StellarItems() {
