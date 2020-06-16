2cd97a87dd21e89cf687ef92c592a63b1f720e4f
Maowcraft <28311382+Maowcraft@users.noreply.github.com>
2020-06-16 10:21:48 -0400

    Added new items
    Also added some loot table injection for the Communicator to spawn in, so that's neat.

--- src/main/java/teamair/stellarcontracts/StellarContracts.java
+++ src/main/java/teamair/stellarcontracts/StellarContracts.java
@@ -1,7 +1,13 @@
 package teamair.stellarcontracts;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.minecraft.loot.ConstantLootTableRange;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.util.Identifier;
+import teamair.stellarcontracts.registry.StellarItems;
 import teamair.stellarcontracts.registry.StellarRegistries;
 
 public class StellarContracts implements ModInitializer {
@@ -10,6 +16,16 @@ public class StellarContracts implements ModInitializer {
     @Override
     public void onInitialize() {
         StellarRegistries.init();
+
+        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) -> {
+            if (new Identifier("chests/simple_dungeon").equals(id)) {
+                LootPool pool = FabricLootPoolBuilder.builder()
+                        .rolls(ConstantLootTableRange.create(1))
+                        .withEntry(ItemEntry.builder(StellarItems.COMMUNICATOR).build()).build();
+
+                supplier.withPool(pool);
+            }
+        });
     }
 
     public static Identifier id(String path) {
--- src/main/java/teamair/stellarcontracts/registry/StellarItems.java
+++ src/main/java/teamair/stellarcontracts/registry/StellarItems.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 import teamair.stellarcontracts.StellarContracts;
 import teamair.stellarcontracts.item.CommunicatorItem;
@@ -13,7 +14,23 @@ public final class StellarItems {
     public static final ItemGroup STELLAR_ITEM_GROUP = FabricItemGroupBuilder.build(StellarContracts.id("item_group"), () -> new ItemStack(StellarItems.COMMUNICATOR));
 
     // Items
+    public static final Item METATANIUM_INGOT = register("metatanium_ingot", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP)));
+    public static final Item METATANIUM_PLATE = register("metatanium_plate", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP)));
     public static final Item COMMUNICATOR = register("communicator", new CommunicatorItem(new Item.Settings()));
+    public static final Item BASIC_CIRCUIT = register("basic_circuit", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP)));
+    public static final Item ADVANCED_CIRCUIT = register("advanced_circuit", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP)));
+    public static final Item ALIEN_CIRCUIT = register("alien_circuit", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP).fireproof().rarity(Rarity.UNCOMMON)));
+    public static final Item CANISTER = register("canister", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP)));
+    public static final Item ACTIVATED_IODZIUM_CANISTER = register("activated_iodzium_canister", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP).maxCount(1)));
+    public static final Item ROCKET_FUEL_CANISTER = register("rocket_fuel_canister", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP).maxCount(1)));
+    public static final Item COMPUTATIONAL_CORE = register("computational_core", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP).fireproof().rarity(Rarity.UNCOMMON)));
+    public static final Item HOLOPROJECTOR = register("holoprojector", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP).maxCount(1)));
+    public static final Item TRACKING_CIRCUIT = register("tracking_circuit", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP)));
+    public static final Item ROCKET_ENGINE = register("rocket_engine", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP).maxCount(1)));
+    public static final Item IODZIUM_CRYSTAL = register("iodzium_crystal", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP).fireproof().rarity(Rarity.UNCOMMON)));
+    public static final Item HOLOSIGHT = register("holosight", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP).maxCount(1)));
+    public static final Item GAUSS_ACCELERATOR = register("gauss_accelerator", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP).maxCount(1)));
+    public static final Item PLASMA_MAGAZINE = register("plasma_magazine", new Item(new Item.Settings().group(STELLAR_ITEM_GROUP).maxCount(6)));
 
     private static Item register(String path, Item item) {
         return Registry.register(Registry.ITEM, StellarContracts.id(path), item);
